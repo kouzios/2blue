0224c4416757ebedb1e3e962d52d8e54657ae987
Matthew Kouzios <mkouzios@hotmail.com>
2020-09-27 06:31:24 -0400

    Added basic sign in/sign out button using Google OAuth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import Decks from './views/Decks';
 import Decklist from './views/Decklist';
 import Home from './views/Home';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 const App = () => {
@@ -12,6 +13,14 @@ const App = () => {
     redirect();
     //eslint-disable-next-line
   }, [view]);
+
+  useEffect(() => {
+    window.gapi.signin2.render("google-sign-in-button", {
+      width: 200,
+      height: 50,
+      onsuccess: onSignIn
+    });
+  })
 
   const openDecklist = (id) => {
     setDeckID(id);
@@ -27,8 +36,26 @@ const App = () => {
     }
   }
 
+  function signOut() {
+    var auth2 = window.gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
   return (
     <div>
+      <div id="google-sign-in-button" data-onsuccess="onSignIn"/>
+      <Button id="google-sign-out-button" onClick={signOut}>Sign Out</Button>
+      <hr/>
       {redirect()}
       <hr/>
       <div className="clickable" onClick={() => setView("home")}> Go Home </div>
