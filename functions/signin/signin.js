c24ae4167d3473a600052d83a2f54eaf0005bb6c
Matthew Kouzios <mkouzios@hotmail.com>
2020-09-30 04:19:45 -0400

    Simplified signin class to call each step as an independant method

--- functions/signin/signin.js
+++ functions/signin/signin.js
@@ -6,25 +6,28 @@ const CLIENT_ID = "461948227597-46gn0e30e3deei7nfnfml771fs7t1a9e.apps.googleuser
 const client = new OAuth2Client(CLIENT_ID);
 const table = base("Users");
 
-const findUser = async (userid) => {
+const userExists = async (userid) => {
     const user = (await table.select({filterByFormula: `OR(userid = '${userid}')` }).firstPage())[0];
     return user !== undefined;
   };
 
-const verify = async (token, fields) => {
+const verify = async (token) => {
     try {
         const ticket = await client.verifyIdToken({
             idToken: token,
             audience: CLIENT_ID,
         });
         const payload = ticket.getPayload();
-        const userid = payload['sub'];
-        const userExists = await findUser(userid);
+        return payload['sub'];;
+    } catch(err) {
+        console.error(err);
+        return null;
+    }
+}
 
-        if(userExists) {
-            return formattedReturn(200, {});
-        }
-        const user = await table.create([{ fields: {userid, ...fields} }]);
+const create = async (fields) => {
+    try {
+        const user = await table.create([{ fields }]);
         return formattedReturn(200, {});
     } catch(err) {
         console.error(err);
@@ -37,7 +40,17 @@ exports.handler =  async (event) => {
         const body = JSON.parse(event.body);
         const auth_token = body.id_token;
         const user = body.user;
-        return verify(auth_token, user);
+        const userid = await verify(auth_token);
+
+        if(!userid) {
+            return formattedReturn(401, {});
+        }
+
+        if(userExists() === true) {
+            return formattedReturn(200, {});
+        } else {
+            return await create({userid, ...user})
+        }
     } else {
         return formattedReturn(405, {});
     }
